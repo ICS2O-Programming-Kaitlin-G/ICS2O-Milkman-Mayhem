4b3255423e41771a223f6e3cbadf49eb9ae398dd
Katie <99366154+kaitlingreen@users.noreply.github.com>
2022-06-10 07:54:55 +0000

    oh man that is some CODE

--- js/gameScene.js
+++ js/gameScene.js
@@ -40,8 +40,28 @@ class GameScene extends Phaser.Scene {
     //this places the sprite on the screen and adds physics to the sprite
     this.milkman = this.physics.add.sprite(1920 / 2, 1080 - 100, 'milkman').setScale(0.2)
   }
-
+  //this update code will attempt to run approximately 60 times a second (predetermined by phaser)
   update (time, delta) {
+    //this initializes a variable to contain the data when the left key is pressed.
+    const keyLeftObj = this.input.keyboard.addKey('LEFT')
+    //this initializes a variable to contain the data when the right key is pressed. 
+    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+    //this checks if the key has been pressed and moves the spaceship to the left if so. 
+    if (keyLeftObj.isDown === true) {
+      this.milkman.x = this.milkman.x - 10
+      //this checks if the sprite is too far left, resulting in the sprite being off the screen, and if so, brings it back to the edge of the screen.
+      if (this.milkman.x < 0) {
+      this.milkman.x = 0
+      }
+    }
+    //this checks if the right key has been pressed and moves the spaceship to the right if so. 
+    if (keyRightObj.isDown === true) {
+      this.milkman.x = this.milkman.x + 10
+      //this checks if the sprite is too far right, resulting in the sprite being off the screen, and if so, brings it back to the edge of the screen
+      if (this.milkman.x > 1920) {
+        this.milkman.x = 1920
+      }
+    }
   }
 }
 
